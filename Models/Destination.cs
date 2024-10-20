6365a8ce06610b1035886773403492e3128284b9
lykasebelina <163415988+lykasebelina@users.noreply.github.com>
2024-10-20 17:15:08 +0800

    Add files via upload

---
+++ Models/Destination.cs
@@ -0,0 +1,11 @@
+﻿namespace BucketListHUB.Models
+{
+    public class Destination
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Language { get; set; }
+        public string Currency { get; set; }
+        public string Citizenship { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BucketListHUB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
